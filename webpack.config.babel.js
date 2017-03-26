596bc23f8541bfbf006b37a5f33a87b0b208a571
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-03-25 21:20:45 -0500

    Added webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,44 @@
+
+// const webpack = require('webpack');
+import path from 'path'
+import webpack from 'webpack'
+
+import { isProd } from './src/shared/util'
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src/client/');
+
+var config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        filename: 'js/bundle.js',
+        path: BUILD_DIR
+    },
+    module : {
+        loaders : [
+            {
+                test    : /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test    : /\.scss?/,
+                loader  : 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    },
+    devtool: isProd ? false : 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+         new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+};
+
+module.exports = config;
